33f85e7665758497e65e53285c2c5d6ad72822bf
saijyothi-19 <spdudam19@gmail.com>
2026-02-20 23:17:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/The New Line - Java/the-new-line-java.java
@@ -0,0 +1,9 @@
+class Geeks {
+
+    // Function to print each word in single line
+    static void printIndividualLine() {
+
+        // Your code here
+        System.out.println("Geeks\nfor\nGeeks");
+    }
+}
