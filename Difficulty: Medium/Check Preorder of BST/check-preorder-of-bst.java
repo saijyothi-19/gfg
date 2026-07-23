2e389b72098f2b171a80dc0e1baa02fb1f01cc0f
saijyothi-19 <spdudam19@gmail.com>
2026-07-23 23:58:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check Preorder of BST/check-preorder-of-bst.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public boolean canRepresentBST(List<Integer> arr) {
+        Stack<Integer> stack = new Stack<>();
+        int lowerBound = Integer.MIN_VALUE;
+
+        for (int value : arr) {
+            if (value < lowerBound) {
+                return false;
+            }
+            while (!stack.isEmpty() && value > stack.peek()) {
+                lowerBound = stack.pop();
+            }
+            stack.push(value);
+        }
+
+        return true;
+    }
+}
